38c8a3b7185cfc03b2d1c6e3c715e5f522df60dd
Greg Shanks <greg.shanks@gmail.com>
2019-11-25 17:16:15 +0000

    Added high level layout of game logic, specifically potential outcomes.

--- models/game.rb
+++ models/game.rb
@@ -1,5 +1,21 @@
 class Game
 
-  
+  # Need to create some logic for the outcome conditions of RPS
+  # All logic to stay in this class, none in the controller
+  # Possible Outcomes of the Game (using URL syntax for later reference)
+  #     if                 then
+  # /rock/scissors      | rock win
+  # /rock/paper         | rock loss
+  # /rock/rock          | draw
+  # /scissors/paper     | scissors win
+  # /scissors/rock      | scissors loss
+  # /scissors/scissors  | draw
+  # /paper/rock         | paper win
+  # /paper/scissors     | paper loss
+  # /paper/paper        | draw
+
+  def self.play()
+    
+  end
 
 end
---
+++ specs/game_spec.rb
@@ -0,0 +1,9 @@
+require 'minitest/autorun'
+require_relative '../models/calculator'
+
+class TestGame < Minitest::Test
+
+  def test1()
+  end
+
+end
