fc0b66dcd6e12a7a174a71876c7f52c8321f1ff1
Greg Shanks <greg.shanks@gmail.com>
2019-11-25 22:50:27 +0000

    Created and began populating views pages. Home, Play and Rules pages navigate to each other. Next step is to add some more text to Home and use Play to run Ruby methods for the game outcomes.

--- app.rb
+++ app.rb
@@ -4,6 +4,22 @@ require('sinatra/contrib/all') if development?
 require_relative('./models/game.rb')
 also_reload('./models/*')
 
-get '/' do
-  return "Nothing here... yet!"
+# initialization for pages in view
+# need a page for every possible outcome - 9
+# homepage: links to 'play' or 'rules' page
+# play page: links (and eventually buttons) for player1 RPS selection
+# rules page: description of RPS rules and a play page link
+# player1 page: links (buttons) for player2 RPS selection and results page
+# results page: display result and provide links to home and play pages
+
+get '/home' do
+  erb(:home)
+end
+
+get '/play' do
+  erb(:play)
+end
+
+get '/rules' do
+  erb(:rules)
 end
